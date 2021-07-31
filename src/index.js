3ccece842ca93cd977f34e476a916b923278fff3
brenorosas <brenorosas@hotmail.com>
2021-07-31 09:20:32 -0300

    feat: add docker'

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const app = express();
-
+const PORT = 3000;
+const HOST = '0.0.0.0';
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(routes);
-app.listen(3000, () => {
+app.listen(PORT, HOST, () => {
     console.log("Started at http://localhost:3000");
 })
